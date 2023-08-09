265a80795feec40da49d2b0efbb1900ca185ba37
nikonovichanna <nikonovicvhhanna@gmail.com>
2023-08-09 21:43:30 +0300

    add task2.3

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿int num = new Random().Next (1, 8);
+System.Console.WriteLine(num); 
+switch (num)
+{
+    case 1:
+        Console.WriteLine("нет");
+        break;
+    case 2:
+        Console.WriteLine("нет");
+        break;
+    case 3:
+        Console.WriteLine("нет");
+        break;
+    case 4:
+        Console.WriteLine("нет");
+        break;
+    case 5:
+        Console.WriteLine("нет");
+        break;
+    case 6:
+        Console.WriteLine("да");
+        break;
+    case 7:
+        Console.WriteLine("да");
+        break;
+    
+}
